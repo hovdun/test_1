fb460f5297d33ea73b393e942bb478319cb860c4
hovdun <92162792+hovdun@users.noreply.github.com>
2022-06-22 11:25:42 +0300

    House

---
+++ House/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+$('.slider').slick({
+nextArrow: '<button class="slick-arrow slick-next"><img src="img/next.svg" alt=""></button>',
+prevArrow:'<button class="slick-arrow slick-prev"><img src="img/prev.svg" alt=""></button>',
+fade:true
+
+});
+});
